8df0ac71ccab91014b1e1bee31b73c69580fcf9b
TTerry8155 <tterry8155@gmail.com>
2017-07-13 12:17:11 -0500

    Modified NewsPaperOrganizaiton

--- Abstractions/NewsPaperOrganization.cs
+++ Abstractions/NewsPaperOrganization.cs
@@ -9,6 +9,16 @@ namespace ObserverPattern
     {
         private List<INewsPaperSubscriber> _subscribers = new List<INewsPaperSubscriber>();
 
+        public string OrganizationName { get; set; }
+
+        public double MonthlySubscriptionCost { get; set; }
+
+        public NewsPaperOrganization(string name, double monthlySubscriptionCost)
+        {
+            OrganizationName = name;
+            MonthlySubscriptionCost = monthlySubscriptionCost;
+        }
+
         public void AttachSubscriber(INewsPaperSubscriber subscriber)
         {
             _subscribers.Add(subscriber);
