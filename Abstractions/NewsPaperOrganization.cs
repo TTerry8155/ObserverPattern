eb187a7f2fce690e6de96706bf4cf82a218262cf
TTerry8155 <tterry8155@gmail.com>
2017-07-13 12:09:45 -0500

    Renamed NewsPaperOrganization and Directory, modified NewsPaperOrganaization to Abstract Class

---
+++ Abstractions/NewsPaperOrganization.cs
@@ -0,0 +1,30 @@
+﻿using ObserverPattern.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObserverPattern
+{
+    public abstract class NewsPaperOrganization
+    {
+        private List<INewsPaperSubscriber> _subscribers = new List<INewsPaperSubscriber>();
+
+        public void AttachSubscriber(INewsPaperSubscriber subscriber)
+        {
+            _subscribers.Add(subscriber);
+        }
+
+        public void DetachSubscriber(INewsPaperSubscriber subscriber)
+        {
+            _subscribers.Remove(subscriber);
+        }
+
+        public void Notify()
+        {
+            foreach (var subscriber in _subscribers)
+            {
+                subscriber.Update();
+            }
+        }
+    }
+}
